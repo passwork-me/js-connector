0be9b6920595b37b48aa205c649678bad017994e
Andrei Sharomov <sharomov.andrei@gmail.com>
2021-01-12 19:08:09 +0300

    Prevent usage master password with clientSideEncryptionDisabled

--- src/rest-modules/users.js
+++ src/rest-modules/users.js
@@ -4,10 +4,15 @@ module.exports = function (options, request, api) {
         options.masterPassword = masterPassword ? masterPassword : false;
         options.useMasterPassword = !!masterPassword;
         return new Promise((resolve, reject) => {
-            request.post(`/auth/login/${apiKey}`).then(data => {
+            request.post(`/auth/login/${apiKey}`, {useMasterPassword: options.useMasterPassword}).then(data => {
                 options.token = data.token;
                 resolve(data);
-            }).catch(err => reject(err));
+            }).catch(err => {
+                if (err.code === 'clientSideEncryptionDisabled') {
+                    err.data.errorMessage = 'Do not use master password with disabled client side encryption'
+                }
+                reject(err);
+            });
         });
     }
 
