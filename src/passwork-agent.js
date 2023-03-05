e9e4d0f406672f17d4d42fd451230fb402d5e2a2
Andrey Sharomov <sharomov.andrei@gmail.com>
2023-03-05 21:41:41 +0600

    revert changes

--- src/passwork-agent.js
+++ src/passwork-agent.js
@@ -1,8 +1,12 @@
 module.exports = function (options) {
-    const axios = require('axios');
-    const fetchAdapter = require("../libs/axios-fetch-adapter");
-    // import fetchAdapter from "@vespaiach/axios-fetch-adapter";
+    const superagent = require("superagent");
     const cryptoInterfaceFactory = require("../libs/crypt");
+    const _method = {
+        'POST':   (ep) => superagent.post(ep),
+        'PUT':    (ep) => superagent.put(ep),
+        'GET':    (ep) => superagent.get(ep),
+        'DELETE': (ep) => superagent.delete(ep)
+    }
 
     this.request = (endpoint, method, body) => new Promise((resolve, reject) => {
         let requestUrl = method + ': ' + options.host + endpoint;
@@ -10,64 +14,56 @@ module.exports = function (options) {
             console.log(requestUrl);
         }
 
-        let params = {
-            url:     options.host + endpoint,
-            method:  method,
-            data:    body,
-            headers: {
-                'Passwork-Auth':       options.token,
-                'Passwork-MasterHash': cryptoInterfaceFactory(options).hash(options.masterPassword),
-                'Passwork-Lang':       options.lang,
-            }
-        };
-        if (options.useFetchApi) {
-            params.adapter = fetchAdapter
-        }
-        axios(params).then(res => resolve(res.data.data))
+        _method[method](options.host + endpoint)
+            .send(body)
+            .set('Passwork-Auth', options.token)
+            .set('Passwork-MasterHash', cryptoInterfaceFactory(options).hash(options.masterPassword))
+            .set('Passwork-Lang', options.lang)
+            .then(res => resolve(res.body.data))
             .catch(err => {
                 err.endpoint = requestUrl;
                 return reject(err);
-            });
+            })
     });
 
-    ['post','put','get','delete'].forEach(method => {
-        this.request[method] = (endpoint, body) =>
-            this.request(endpoint, method, body)
-                .catch(this.handleApiSessionExpired)
-                .then((data) => data === 'retry' ? this.request(endpoint, key, body) : data)
-                .catch(this.handleError)
-    });
+    for (const key in _method) {
+      this.request[key.toLowerCase()] = (endpoint, body) =>
+        this.request(endpoint, key, body)
+          .catch(this.handleApiSessionExpired)
+          .then((data) => data === 'retry' ? this.request(endpoint, key, body) : data)
+          .catch(this.handleError)
+    }
 
     this.handleApiSessionExpired = (e) => {
-        if (!!options.refreshToken && e.response && e.response.body && e.response.body.code === 'apiSessionExpired') {
-            const token = options.token;
-            options.token = '';
+      if (!!options.refreshToken && e.response && e.response.body && e.response.body.code === 'apiSessionExpired') {
+        const token = options.token;
+        options.token = '';
 
-            return this.request(`/auth/refreshToken/${token}/${options.refreshToken}`, 'POST').then(response => {
-                options.token = response.token;
-                options.refreshToken = response.refreshToken;
+        return this.request(`/auth/refreshToken/${token}/${options.refreshToken}`, 'POST').then(response => {
+          options.token = response.token;
+          options.refreshToken = response.refreshToken;
 
-                return Promise.resolve('retry');
-            })
-        }
+          return Promise.resolve('retry');
+        })
+      }
 
-        return Promise.reject(e);
+      return Promise.reject(e);
     }
 
     this.handleError = (e) => {
-        if (e.response && e.response.body) {
-            throw {
-                httpRequest: e.endpoint,
-                httpStatus:  e.status,
-                httpMessage: e.message,
-                ...e.response.body
-            };
-        } else {
-            throw {
-                httpRequest: e.endpoint,
-                httpStatus:  'serverError',
-                httpMessage: 'serverError',
-            };
-        }
+      if (e.response && e.response.body) {
+        throw {
+          httpRequest: e.endpoint,
+          httpStatus:  e.status,
+          httpMessage: e.message,
+          ...e.response.body
+        };
+      } else {
+        throw {
+          httpRequest: e.endpoint,
+          httpStatus:  'serverError',
+          httpMessage: 'serverError',
+        };
+      }
     }
 };
